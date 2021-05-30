5da3c891e9d5d57d8d5a9525550b904e42a3666a
Takumikimura141899 <kimura141899@gmail.com>
2021-05-30 13:17:04 +0900

    second-commit

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import Form from "./Form";
 import List from "./List";
 
 const App = () => {
-  const todos = [
+  const [todos, setTodos] = useState([
     { content: "課題をする" },
     { content: "課題をする" },
     { content: "課題をする" },
     { content: "課題をする" },
-  ];
+  ]);
 
   return (
     <>
       <h1>ToDo App</h1>
-      <Form />
+      <Form todos={todos} setTodos={setTodos} />
       <List todos={todos} />
     </>
   );
--- src/Form.js
+++ src/Form.js
@@ -1,9 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Form = () => {
+const Form = ({ todos, setTodos }) => {
+  const [value, setValue] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log("value:", value);
+    if (value === "") {
+      alert("文字を入力してください");
+      return;
+    }
+    setTodos([...todos, { content: value }]);
+    setValue("");
+  };
+
   return (
-    <form>
-      <input type="text" />
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        onChange={(event) => {
+          setValue(event.target.value);
+        }}
+        value={value}
+      />
     </form>
   );
 };
